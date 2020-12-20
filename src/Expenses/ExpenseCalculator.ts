452ba0bfecc258cc1985a829605869b154eb7e4a
drsherlock <zain.delhi27@hotmail.com>
2020-12-21 00:39:25 +0530

    Complete evalute method

--- src/Expenses/ExpenseCalculator.ts
+++ src/Expenses/ExpenseCalculator.ts
@@ -3,19 +3,61 @@ import IPerson from "../Person/IPerson";
 
 class ExpenseCalculator {
   static calculate(
-    personsLedger: Map<IPerson, number>,
+    ledger: Map<IPerson, number>,
     expense: IExpense
   ): void {
-    for (const [person, amount] of personsLedger) {
-      personsLedger.set(person, expense.splitMap.get(person)! + amount);
+    for (const [person, amount] of ledger) {
+      ledger.set(person, expense.splitMap.get(person)! + amount);
     }
   }
 
+  // [
+  //   { name: Person { name: 'b' }, value: -83.33 },
+  //   { name: Person { name: 'c' }, value: -83.33 },
+  //   { name: Person { name: 'a' }, value: 166.66 }
+  // ]
   static evaluate(
-    personsLedger: Map<IPerson, number>
-  ): Array<Record<string, unknown>> {
-    //
-    return [{}];
+    ledger: Map<IPerson, number>
+  ): Array<string> {
+  	const evaluatedLedger: Array<string> = [];
+    const ledgerArray = Array.from(ledger, ([person, amount]) => ({ person, amount }));
+    const ledgerArraySorted = ledgerArray.sort((a, b) => a.amount - b.amount);
+
+    let left_idx = 0;
+    let right_idx = ledgerArraySorted.length - 1;
+
+    while (left_idx < right_idx) {
+    	// [{a, -5}, {b, 5}]
+    	if (ledgerArraySorted[left_idx].amount === -ledgerArraySorted[right_idx].amount) {
+    		evaluatedLedger.push(`${ledgerArraySorted[right_idx].person.name} gets back ${ledgerArraySorted[right_idx].amount} from ${ledgerArraySorted[left_idx].person.name}`);
+
+    		ledgerArraySorted[left_idx].amount = 0;
+    		ledgerArraySorted[right_idx].amount = 0;
+
+    		left_idx += 1;
+    		right_idx -= 1;
+    	}
+    	// [{a, -5}, {b, 3}]
+    	else if (ledgerArraySorted[left_idx].amount < -ledgerArraySorted[right_idx].amount) {
+    		evaluatedLedger.push(`${ledgerArraySorted[right_idx].person.name} gets back ${ledgerArraySorted[right_idx].amount} from ${ledgerArraySorted[left_idx].person.name}`);
+
+    		ledgerArraySorted[left_idx].amount = ledgerArraySorted[left_idx].amount + ledgerArraySorted[right_idx].amount;
+    		ledgerArraySorted[right_idx].amount = 0;
+
+    		right_idx -= 1;
+    	}
+    	// [{a, -3}, {b, 5}]
+    	else if (ledgerArraySorted[left_idx].amount > -ledgerArraySorted[right_idx].amount) {
+    		evaluatedLedger.push(`${ledgerArraySorted[right_idx].person.name} gets back ${-ledgerArraySorted[left_idx].amount} from ${ledgerArraySorted[left_idx].person.name}`);
+
+    		ledgerArraySorted[right_idx].amount = ledgerArraySorted[left_idx].amount + ledgerArraySorted[right_idx].amount;
+    		ledgerArraySorted[left_idx].amount = 0;
+
+    		left_idx += 1;
+    	}
+    }
+
+    return evaluatedLedger;
   }
 }
 
--- src/Expenses/Expenses.ts
+++ src/Expenses/Expenses.ts
@@ -5,19 +5,23 @@ import IPerson from "../Person/IPerson";
 
 class Expenses implements IExpenses {
   // private expensesLog: IExpenses[];
-  private personsLedger: Map<IPerson, number> = new Map<IPerson, number>();
+  private ledger: Map<IPerson, number> = new Map<IPerson, number>();
   constructor(personsMap: Map<string, IPerson>) {
     for (const [_, person] of personsMap) {
-      this.personsLedger.set(person, 0);
+      this.ledger.set(person, 0);
     }
   }
 
   add(expense: IExpense): void {
-    ExpenseCalculator.calculate(this.personsLedger, expense);
+    ExpenseCalculator.calculate(this.ledger, expense);
   }
 
   show(): void {
-    console.log(this.personsLedger);
+    const evaluatedLedger: Array<string> = ExpenseCalculator.evaluate(this.ledger);
+
+    for(let i = 0; i < evaluatedLedger.length; i++) {
+      console.log(evaluatedLedger[i]);
+    }
   }
 }
 
